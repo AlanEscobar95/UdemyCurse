c0c5043eb776569d5d73de37d86dcc493492242d
AlanEscobar95 <arp.escobar@yavirac.edu.ec>
2021-10-09 14:20:25 -0500

    Udemy_Curse

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,57 @@
+ @extends('layouts.main')
+ @section('contenido')
+ <div class="container"><br>
+     <div class="row">
+         <div class="col-md-12">
+             <div class="card">
+                 <div class="hard-header">
+                     Listado de productos
+                     <a href="{{ route ('products.create') }}"class="btn btn-success btn-sm float-right"> Nuevo Producto </a>
+                 </div>
+                 <div class="card-body">
+                     @if(session('info'))
+                     <div class="alert alert-success">
+                        {{ session('info') }}
+                    </div>
+                    @endif
+                     <table class="table table-hover table-sm">
+                           <head>
+                               <th>Descripcion</th>
+                               <th>Precio</th>
+                               <th>Acción</th>
+                           </head>
+
+                           <tbody>
+                               @foreach($products as $product)
+                                <tr> 
+                                    <td>
+                                       {{$product->description}}
+                                    </td>
+                                    <td>
+                                       {{$product->price}} 
+                                    </td>
+                                    <td>
+                                       <a href="{{ route ('products.edit', $product->id)}}" class="btn btn-warning btn-sm">Editar</a>
+                                       <a href="javasript:document.getElementById('delete-{{$product->id}}')" class="btn btn-danger btn-sm">Eliminar</a> 
+                                       <form id="delete-{{$product->id}}" action="{{route('products.destroy',$product->id)}}" method="POST">
+                                         @method('delete')
+                                         @csrf
+                                       </form>    
+                                    </td>
+                                </tr>
+                                @endforeach
+                           </tbody>
+                     </table>
+                 </div> 
+                 <div class="card-footer">
+                     Bienvenido {{auth()->user()->name}}
+                     <a href="javascript:document.getElementById('logout').submit()" class="btn btn-danger btn-sm float-right">Cerrar sesión</a>
+                     <form action="{{route('logout')}}" id="logout" style="display:none" method="post">
+                     @csrf
+                     </form>
+                </div>   
+             </div>
+        </div>
+     </div>
+</div>
+@endsection
